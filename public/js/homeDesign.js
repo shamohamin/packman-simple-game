e71d26afd298e9d8efe4948dfd4b54e34fd504b1
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-04-10 03:33:17 +0430

    remove drawHome function

--- public/js/homeDesign.js
+++ public/js/homeDesign.js
@@ -5,7 +5,6 @@ import { eatingNodes } from "./gameLogic.js";
 
 window.onload = function () {
     const ctx = this.document.getElementById('canvas').getContext('2d');
-    drawHome(ctx);
     ctx.fillStyle="yellow";
     write_packman();
     let staff = [];
@@ -41,25 +40,6 @@ window.onload = function () {
     audio.style.display = 'none';
 
 
-}
-
-const drawHome = (ctx) => {
-    ctx.strokeStyle = 'red';
-    ctx.beginPath();
-
-    ctx.moveTo(200, 20);
-    ctx.lineTo(20, 150);
-    ctx.lineTo(20, 380);
-    ctx.lineTo(180, 380);
-    ctx.lineTo(180, 330);
-    ctx.lineTo(220, 330);
-    ctx.lineTo(220, 380);
-    ctx.lineTo(380, 380);
-    ctx.lineTo(380, 150);
-    ctx.lineTo(200,20);
-    
-    ctx.stroke();
-    ctx.closePath();
 }
 
 const animated = (ctx, staff, packman, packman_move) => {
