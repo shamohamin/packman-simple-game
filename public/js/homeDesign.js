9adf72e067add17123323e10b9790121294ca804
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-04-10 01:58:26 +0430

    added packman animated to home page

--- public/js/homeDesign.js
+++ public/js/homeDesign.js
@@ -1,7 +1,40 @@
+import { Packman, Circle } from "./gameObjects.js";
+import { RIGHT, LEFT } from "./actionsAndGameConstants.js";
+import { eatingNodes } from "./gameLogic.js";
+
+
 window.onload = function () {
     const ctx = this.document.getElementById('canvas').getContext('2d');
     drawHome(ctx);
-    const audio = new this.Audio('../public/sounds/pacman_beginning.wav');
+    ctx.fillStyle="yellow";
+    
+    let staff = [];
+    for(let i = 170 ; i <= 260 ; i += 10)
+        staff.push(new Circle(i, 150))
+    
+    let packman_move = RIGHT;
+    const packman = new Packman(150,150);
+    staff.push(packman);
+    
+    let direction_right = 10; 
+    let staff_cop = this.Object.assign([], staff) ;
+    setInterval(() => {
+        
+        if(packman.x > 250){
+            direction_right = -10 ;
+            packman_move = LEFT;
+            staff_cop = Object.assign([] , staff);
+        }else if(packman.x < 150){
+            direction_right = 10 ;
+            packman_move = RIGHT;
+            staff_cop = Object.assign([] , staff);
+        }
+
+        packman.setAtr(packman.x + direction_right, packman.y);
+        animated(ctx,staff_cop,packman,packman_move);
+    }, 500);
+
+    const audio = new this.Audio('../sounds/pacman_beginning.wav');
 
     this.document.body.appendChild(audio);
     // this.body.appendChild(audio);
@@ -26,6 +59,21 @@ const drawHome = (ctx) => {
     ctx.lineTo(200,20);
     
     ctx.stroke();
-
     ctx.closePath();
+}
+
+const animated = (ctx, staff, packman, packman_move) => {
+    eatingNodes(packman.x, packman.y, staff);
+    draw_objects(ctx,staff, packman_move);
+}
+
+const draw_objects = (ctx,staff, packman_move) => {
+
+    ctx.clearRect(130, 130, 150 , 150);
+    for(let item of staff){
+        if(item != undefined)
+            item.draw(ctx)
+        if(item instanceof Packman)
+            item.draw(ctx, packman_move)
+    }
 }
